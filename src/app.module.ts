4fbbdc2a54c5f9c98f37829490a93a63826e9de1
Ân Trần Bảo <20520377@ms.uit.edu.vn>
2023-04-15 16:51:40 +0700

    feat: setup constant

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+
+
 @Module({
-  imports: [],
+  imports: [
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `./config/env/${process.env.NODE_ENV}.env`,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
 })
 export class AppModule {}
